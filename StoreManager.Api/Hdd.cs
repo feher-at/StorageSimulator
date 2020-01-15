46cf9e4ed9cdb01e7f0130c2bcfe6a300c8e5311
makakoocelot <makakoocelot@gmail.com>
2020-01-15 17:07:32 +0100

    Made the hdd class and inherit from the storage class

---
+++ StoreManager.Api/Hdd.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.Api
+{
+    public class Hdd : Storage
+    {
+        public override double FreeCapacity
+        {
+            get
+            {
+                double freecap = this.maxCapacity;
+                foreach (File element in this.fileList)
+                {
+                    freecap -= element.FileSize;
+                }
+                this.freeCapacity = freecap;
+                return this.freeCapacity;
+            }
+        }
+        public override double ReservedCapacity
+        {
+            get
+            {
+                double reservedCap = 0;
+                foreach (File element in this.fileList)
+                {
+                    reservedCap += element.FileSize;
+
+                }
+                this.reservedCapacity = reservedCap;
+                return this.reservedCapacity;
+            }
+
+        }
+        public Hdd(string Id, int MaxCapacity, string StoreName) : base(Id, MaxCapacity, StoreName)
+        {
+        }
+    }
+}
