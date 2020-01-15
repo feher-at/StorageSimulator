b35649ad0c085caea329409fb001b9b6f8c67267
makakoocelot <makakoocelot@gmail.com>
2020-01-15 17:16:21 +0100

    implement the DvD-RW and the DVD class and their methods

---
+++ StoreManager.Api/DVD.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreManager.Api
+{
+    public class DVD : Storage
+    {
+
+        protected bool readOnly;
+        protected bool ReadOnly
+        {
+            get { return readOnly; }
+            set { this.readOnly = value; }
+        }
+        public override double FreeCapacity
+        {
+            get
+            {
+                double freecap = this.maxCapacity;
+                foreach (File element in this.fileList)
+                {
+                    freecap -= element.FileSize;
+                }
+                this.freeCapacity = freecap;
+                return this.freeCapacity;
+            }
+        }
+        public override double ReservedCapacity
+        {
+            get
+            {
+                double reservedCap = 0;
+                foreach (File element in this.fileList)
+                {
+                    reservedCap += element.FileSize;
+
+                }
+                this.reservedCapacity = reservedCap;
+                return this.reservedCapacity;
+            }
+        }
+
+
+        public DVD(string Id, string Name) : base(Id, Name)
+        {
+
+            this.Id = Id;
+            this.maxCapacity = 4.58984375;
+            this.ReadOnly = false;
+        }
+
+        public void Block()
+        {
+            this.ReadOnly = true;
+            this.freeCapacity = 0;
+        }
+
+        public override void Format()
+        {
+            if (this.ReadOnly == true)
+                throw new Exception("The DvD has been blocked sorry");
+            else
+            {
+                base.Format();
+            }
+        }
+
+        public override void AddFile(string fileName, double fileSize, bool onlyRead, string system, bool hidden)
+        {
+            if (ReadOnly == true)
+            {
+                throw new Exception("The DvD has been blocked sorry");
+            }
+            double DvdFileSize = fileSize / 1048576;
+            base.AddFile(fileName, DvdFileSize, onlyRead, system, hidden);
+
+        }
+
+        public override void Remove(string fileName)
+        {
+            if (this.ReadOnly == true)
+            {
+                throw new Exception("There is not enough free capacity");
+            }
+            else
+            {
+                base.Remove(fileName);
+            }
+
+
+
+
+        }
+        public new void listFile()
+        {
+            foreach (File element in fileList)
+                Console.WriteLine(element.ToString());
+        }
+
+
+    }
+}
---
+++ StoreManager.Api/DvD-RW.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreManager.Api
+{
+    public class DvD_RW : DVD
+    {
+        public DvD_RW(string Id, string Name) : base(Id, Name)
+        {
+            this.StoreName = Name;
+        }
+        public void Open()
+        {
+            if (ReadOnly == false)
+                throw new Exception("The DvD is already Open");
+            else
+            {
+                this.ReadOnly = false;
+                this.fileList = new List<File>();
+            }
+        }
+
+
+    }
