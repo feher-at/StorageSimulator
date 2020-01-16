55ac06a9d8f8f75fbfaadb870ab825b51f2121c4
makakoocelot <makakoocelot@gmail.com>
2020-01-16 11:03:04 +0100

    declare the Ilogger class's methods and implement in the ConsoleLogger class

---
+++ StoreSimulator/ConsoleLogger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreSimulator
+{
+    class ConsoleLogger:ILogger
+    {
+        public void Error(string error)
+        {
+            string err = "ERROR";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(err);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(" " + error);
+
+        }
+        
+        public void UserInput(string userinput)
+        {
+            string input = "INPUT";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(input);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(" " + userinput);
+        }
+        public void ConsoleInfo(string Info)
+        {
+            string info = "INFO";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(info);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(" " + Info);
+
+        }
+    }
+}
---
+++ StoreSimulator/Ilogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreSimulator
+{
+    interface ILogger
+    {
+        void Error(string message);
+
+        void UserInput(string message);
+
+        void ConsoleInfo(string message);
+
+
+    }
+}
